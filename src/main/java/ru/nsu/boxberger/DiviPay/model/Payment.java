4fd11e49b3a2f6c9c6a44a059fed546d88511bac
ArtemBoxberger <coder1712@gmail.com>
2023-06-08 12:30:26 +0700

    update

--- src/main/java/ru/nsu/boxberger/DiviPay/model/Payment.java
+++ src/main/java/ru/nsu/boxberger/DiviPay/model/Payment.java
@@ -15,39 +15,4 @@ public class Payment {
 
     @Column(name = "user_id")
     private Long userID;
-
-    @Column(name = "is_paid")
-    private Boolean isPaid;
-
-    public Long getPaymentID() {
-        return paymentID;
-    }
-
-    public void setPaymentID(Long paymentID) {
-        this.paymentID = paymentID;
-    }
-
-    public Long getPurchaseID() {
-        return purchaseID;
-    }
-
-    public void setPurchaseID(Long purchaseID) {
-        this.purchaseID = purchaseID;
-    }
-
-    public Long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Long userID) {
-        this.userID = userID;
-    }
-
-    public Boolean getPaid() {
-        return isPaid;
-    }
-
-    public void setPaid(Boolean paid) {
-        isPaid = paid;
-    }
 }
